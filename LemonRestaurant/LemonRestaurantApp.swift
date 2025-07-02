7c2d5035e334cb8fc9d588ea1d8ca39f92f1d905
robert <robert.nikolas.torres@gmail.com>
2025-07-02 15:12:50 -0400

    Initial Commit

---
+++ LemonRestaurant/LemonRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantApp.swift
+//  LemonRestaurant
+//
+//  Created by Robert Nikolas Torres on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
