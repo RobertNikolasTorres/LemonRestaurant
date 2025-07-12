7348b013bc66bb87fdb12a711564bc4967771fa6
robert <robert.nikolas.torres@gmail.com>
2025-07-12 07:01:02 -0400

    102 - Ch1 Assignment 1

---
+++ LemonRestaurant/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  LemonRestaurant
+//
+//  Created by Robert Nikolas Torres on 7/9/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
